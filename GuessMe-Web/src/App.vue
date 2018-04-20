6df76ca0930889c31a7578acad5dcee41a64ecb7
乔丁 <qiaoding@xiyou3g.com>
2018-04-20 17:21:04 +0800

    Create webpage with vue

---
+++ GuessMe-Web/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import indexPage from '@/components/index-page/index-page'
+  import store from '@/store'
+  export default {
+    name: 'app',
+    components: {
+      'index-page': indexPage
+    },
+    store
+  }
+</script>
+
+<style scoped>
+  #app{
+    color: #666;
+  }
+</style>
---
+++ GuessMe-Web/src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as type from './mutation-types'
+
+const state = {
+  id: '',
+  token: ''
+};
+
+const mutations = {
+  [type.CHANGE_ID] (state, newValue){
+    state.id = newValue;
+  },
+  [type.CHANGE_TOKEN] (state, newValue){
+    state.token = newValue;
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  mutations
+})
